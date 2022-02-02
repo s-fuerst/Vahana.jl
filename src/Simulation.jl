060851e4fba7d91adadb5b96964072764b92b08b
sfuerst <fuerst@zib.de>
2022-02-02 11:45:12 +0100

    current/next index is the same for all types

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -5,27 +5,37 @@ export finish_init!
 # Ist Abstractvector wirklich die sinnvolle Basisklasse
 # Ich verlange ja vor allem, dass das Table-Interface unterstützt wird.
 
-Base.@kwdef mutable struct Simulation13
-    name::String
-    params::Union{Tuple, NamedTuple}
-    # Any will be something like Vahala_Agent_Collection
-    agent_data_t = Dict{DataType, Any}()
-    agent_data_tp1 = Dict{DataType, Any}()
-    edge_data_t = Dict{DataType, Any}()
-    edge_data_tp1 = Dict{DataType, Any}()
+const MAX_STEPS = 2 
+
+#TODO: umbauen
+Base.@kwdef struct IDFactory
     id_counter = Dict{DataType, T_AgentNr}()
     type2number = Dict{DataType, T_TypeNumber}()
     number2type = Vector{DataType}(undef, typemax(T_TypeNumber))
+end
+
+
+
+Base.@kwdef mutable struct Simulation15
+    name::String
+    params::Union{Tuple, NamedTuple}
+    # Any will be something like Vahala_Agent_Collection
+    agents = fill(Dict{DataType, Any}(), MAX_STEPS)
+    edges = fill(Dict{DataType, Any}(), MAX_STEPS)
+    id_counter = Dict{DataType, T_AgentNr}()
+    type2number = Dict{DataType, T_TypeNumber}()
+    number2type = Vector{DataType}(undef, typemax(T_TypeNumber))
+    current = 1
+    next = 1
 end
 
 # TOOD: when simulation structure is fixed, maybe make this
 # an inner constructor
 function Simulation(name::String,
              params::Union{Tuple, NamedTuple})
-    sim = Simulation13(name = name,
+    sim = Simulation15(name = name,
                        params = params)
-#    push!(sim.edge_data_t, StatelessEdge => Vector{StatelessEdge})
-    push!(sim.edge_data_tp1, StatelessEdge => Vector{StatelessEdge})
+    push!(sim.edges[sim.next], StatelessEdge => Vector{StatelessEdge})
     
     #    add_edgetype!(sim, StatelessEdge(::UInt64, ::UInt64))
     sim
@@ -40,8 +50,8 @@ function add_agenttype!(sim, ::Type{T}) where {T} #where { T <: Agent }
     # TODO: was ist mit privaten Daten
     type_number = reduce(max, values(sim.type2number); init = 0) + 1
     # TODO: assert type_number ist klein genug
- #   push!(sim.agent_data_t, T => Dict{T_AgentID, T}())
-    push!(sim.agent_data_tp1, T => Dict{T_AgentID, T}())
+ #   push!(sim.agents_t, T => Dict{T_AgentID, T}())
+    push!(sim.agents[sim.next], T => Dict{T_AgentID, T}())
     push!(sim.id_counter, T => 0)
     push!(sim.type2number, T => type_number)
     sim.number2type[type_number] = T
@@ -49,22 +59,23 @@ function add_agenttype!(sim, ::Type{T}) where {T} #where { T <: Agent }
 end
 
 function add_edgetype!(sim, ::Type{T}) where { T } 
-#    push!(sim.edge_data_t, T => Vector{T})
-    push!(sim.edge_data_tp1, T => Dict{T_AgentID,  Vector{Edge{T}}}())
+#    push!(sim.edges_t, T => Vector{T})
+    push!(sim.edges[sim.next],
+          T => Dict{T_AgentID,  Vector{Edge{T}}}())
 end
 
-function add_agents!(sim::Simulation13, agent::T) where { T <: Agent }
+function add_agents!(sim::Simulation15, agent::T) where { T <: Agent }
     id = agentId(sim.type2number[T], sim.id_counter[T])
     sim.id_counter[T] = sim.id_counter[T] + 1
-    sim.agent_data_tp1[T][id] = agent
+    sim.agents[sim.next][T][id] = agent
     id
 end
 
-function add_agents!(sim::Simulation13, agents)
+function add_agents!(sim::Simulation15, agents)
     [ add_agents!(sim, a) for a in agents ]
 end
 
-function add_agents!(sim::Simulation13, agents...)
+function add_agents!(sim::Simulation15, agents...)
     [ add_agents!(sim, a) for a in agents ]
 end
 
@@ -72,7 +83,7 @@ add_agents!(f::Function) = sim -> add_agents!(sim, f(sim))
 
 
 function add_edge!(sim, edge::Edge{T}) where { T }
-    dict = get!(sim.edge_data_tp1[T], edge.to, Vector{Edge{T}}())
+    dict = get!(sim.edges[sim.next][T], edge.to, Vector{Edge{T}}())
     push!(dict, edge)
 end
 
@@ -81,13 +92,19 @@ function add_edge!(sim, from::T_AgentID, to::T_AgentID, state::T) where { T }
 end
 
 function finish_init!(sim)
-    sim.agent_data_t = sim.agent_data_tp1
-    for (k, v) in sim.agent_data_tp1
-        sim.agent_data_tp1[k] = typeof(v)()
+    sim.next = 2
+    # sim.agents_t = sim.agents_tp1
+    for (k, v) in sim.agents[sim.current]
+        sim.agents[sim.next][k] = typeof(v)()
     end
 
-    # sim.edge_data_t = sim.edge_data_tp1
-    # for (k, v) in sim.edge_data_tp1
-    #     sim.edge_data_tp1[k] = typeof(v)()
+    for (k, v) in sim.edges[sim.current]
+        sim.edges[sim.next][k] = typeof(v)()
+    end
+
+
+    # sim.edges_t = sim.edges_tp1
+    # for (k, v) in sim.edges_tp1
+    #     sim.edges_tp1[k] = typeof(v)()
     # end
 end 
--- test/runtests.jl
+++ test/runtests.jl
@@ -29,29 +29,29 @@ end
     h2 = HH(2)
 
     add_agenttype!(sim, Person)
-    @test length(sim.agent_data_tp1) == 1
+    @test length(sim.agents[sim.next]) == 1
     @test sim.type2number[Person] == 1
     @test sim.number2type[1] == Person
 
     add_agenttype!(sim, HH)
-    @test length(sim.agent_data_tp1) == 2
+    @test length(sim.agents[sim.next]) == 2
     @test sim.type2number[HH] == 2
     @test sim.number2type[2] == HH
 
     p1id = add_agents!(sim, p1)
-    @test length(sim.agent_data_tp1[Person]) == 1
-    @test sim.agent_data_tp1[Person][p1id] == p1
+    @test length(sim.agents[sim.next][Person]) == 1
+    @test sim.agents[sim.next][Person][p1id] == p1
 
     ids = add_agents!(sim, p1, p2)
     p2id = ids[2]
-    @test length(sim.agent_data_tp1[Person]) == 3
-    @test sim.agent_data_tp1[Person][p2id] == p2
+    @test length(sim.agents[sim.next][Person]) == 3
+    @test sim.agents[sim.next][Person][p2id] == p2
 
     ids = add_agents!(sim, [[p1, p2], h1])
-    @test length(sim.agent_data_tp1[Person]) == 5
-    @test length(sim.agent_data_tp1[HH]) == 1
-    @test sim.agent_data_tp1[Person][ids[1][1]] == p1
-    @test sim.agent_data_tp1[HH][ids[2]] == h1
+    @test length(sim.agents[sim.next][Person]) == 5
+    @test length(sim.agents[sim.next][HH]) == 1
+    @test sim.agents[1][Person][ids[1][1]] == p1
+    @test sim.agents[sim.next][HH][ids[2]] == h1
 
     h2id = add_agents!(sim, h2)
 
@@ -62,17 +62,16 @@ end
     add_edge!(sim, p1id, h2id, FooEdgeState(1))
     add_edge!(sim, Edge(h2id, p2id, FooEdgeState(2)))
 
-    @test sim.edge_data_tp1[FooEdgeState] |> length == 2
-    @test sim.edge_data_tp1[FooEdgeState][h2id] |> length == 1 
-    @test sim.edge_data_tp1[FooEdgeState][p2id] |> length == 2
+    @test sim.edges[sim.next][FooEdgeState] |> length == 2
+    @test sim.edges[sim.next][FooEdgeState][h2id] |> length == 1 
+    @test sim.edges[sim.next][FooEdgeState][p2id] |> length == 2
     #    add_agents!(sim, createPersons)
 
     finish_init!(sim)
-    @test length(sim.agent_data_t[Person]) == 5
-    # @test length(sim.agent_data_tp1[Person]) == 0
+    @test length(sim.agents[sim.next][Person]) == 0
 
-    # @test sim.edge_data_t[FooEdgeState] |> length == 2
-    # @test sim.edge_data_tp1[FooEdgeState] |> length == 0
+    # @test sim.edges_t[FooEdgeState] |> length == 2
+    # @test sim.edges_tp1[FooEdgeState] |> length == 0
 end
 
 
