0526baf0966032dc62835708f7b958b3dd6d354c
Steffen Fürst <fuerst@zib.de>
2022-09-27 13:21:19 +0200

    Added reuse tests

---
+++ test/reuse.jl
@@ -0,0 +1,36 @@
+struct ReuseAgent end
+
+CHECKLIVING verbessern und überdenken (besser in der anderen Reihenfolge)
+
+sim = ModelTypes() |>
+    register_agenttype!(ReuseAgent, :CheckLiving) |>
+    construct_model("Reuse2") |> 
+    new_simulation(nothing, nothing)
+
+add_agent!(sim, ReuseAgent())
+
+finish_init!(sim)
+
+function add_new_remove_me(_, id, sim)
+    add_agent!(sim, ReuseAgent())
+    nothing
+end
+
+apply_transition!(sim, add_new_remove_me, [ ReuseAgent ], [], [])
+
+@test num_agents(sim, ReuseAgent) == 1
+
+@test sim.ReuseAgent_died[1] == true
+
+@test length(sim.ReuseAgent_read.reuseable) == 1
+
+apply_transition!(sim, add_new_remove_me, [ ReuseAgent ], [], [])
+
+@test num_agents(sim, ReuseAgent) == 1
+
+# the agent should be reused now
+@test sim.ReuseAgent_died[1] == false
+
+@test sim.ReuseAgent_died[2] == true
+
+@test length(sim.ReuseAgent_read.reuseable) == 1
