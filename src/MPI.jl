99a5337524a61eadc6ea4cefcb310d9cb077b42e
Steffen Fürst <fuerst@zib.de>
2022-12-12 16:27:41 +0100

    optimized send_edges!

--- src/MPI.jl
+++ src/MPI.jl
@@ -125,73 +125,6 @@ function sendagents!(sim, perPE::Vector{Vector{AgentID}}, T::DataType)
     idmapping
 end
 
-# For sending the edges we need different versions, depending on the
-# edge traits, as e.g. for the :HasEdgeOnly or :NumEdgeOnly we
-# transfer only the number of edges. In overall, when we iterate over
-# the container, we get the following values:
-# |                    | Statel. | Ignore | get edges via | sending (ST below) |
-# |--------------------+---------+--------+---------------+--------------------|
-# | (Vector){Edge{$T}} |         |        | edges_to      | [(toid, Edge{$T})] |
-# | (Vector){AgentID}  | x       |        | neighborids   | [(toid, fromid)]   |
-# | (Vector){$T}       |         | x      | edgestates    | [(toid, $T)]       |
-# | Int64              | x       | x      | num_neighbors | num_neighbors      |
-function sendedges!(sim, sendmap::Dict{AgentID, ProcessID}, idmapping, T::DataType)
-    ST = Vector{Tuple{AgentID, sim.typeinfos.edges_attr[T][:containerelement]}}
-    # We construct for each PE a vector with all the edges (incl. toid) that
-    # should be transmit to the PE
-    perPE = [ ST() for _ in 1:mpi.size ]
-
-    # The iterator for the edges depends on the traits of the edgetype
-    if has_trait(sim, T, :Stateless) && has_trait(sim, T, :IgnoreFrom)
-        iter = edgeread(sim, T)
-        if has_trait(sim, T, :SingleAgentType)
-            iter = enumerate(iter)
-        end
-    else
-        iter = edges_iterator(sim, T)
-    end
-
-    # we now iterate over the edges and add them to the perPE vector
-    # that corresponds to the process id given in the sendmap for the
-    # target of the edge
-    for (to, e) in iter
-        id = if has_trait(sim, T, :SingleAgentType)
-            AT = sim.typeinfos.edges_attr[T][:to_agenttype]
-            typeID = sim.typeinfos.nodes_type2id[AT]
-            # we don't know the reuse value of the agent in the
-            # :SingleAgentType case, so we call immortal_agent_id,
-            # which uses the reuse value of 0.  This is okay, as we
-            # have removed the reuse bits from the AgentID already
-            # before in sendagents!
-            immortal_agent_id(typeID, AgentNr(to))
-        else
-            # To be consistent with sendagents!, we must remove
-            # the reuse bits here too
-            remove_reuse(to)
-        end
-        # in the default finish_init! case with an initialization
-        # that does not care about mpi.isroot, we have edges on
-        # all ranks, but on the sendmap there are only the edges
-        # of rank 0, as this are the only edges we want to distribute
-        if haskey(sendmap, id)
-            # in the SingleAgentType version, we also get entries with 0 edges
-            # we skip them, there is no need to use bandwith for that
-            if has_trait(sim, T, :Stateless) &&
-                has_trait(sim, T, :IgnoreFrom) &&
-                e == 0
-                continue
-            end
-            push!(perPE[sendmap[id]], (id, e))
-        end
-    end
-
-    # get the updated id of an agent with the old id `id`, whereby the
-    # reuse bits of `id` are set to zero (as this is also the case for
-    # the keys of idmapping)
-    updateid(id::AgentID) = idmapping[id] |> AgentID
-    
-    edges_alltoall!(sim, perPE, T, updateid)
-end
 
 function construct_mpi_agent_methods(T::DataType, attr, simsymbol, checkliving)
     stateless = :Stateless in attr[:traits]
@@ -223,8 +156,8 @@ function construct_mpi_agent_methods(T::DataType, attr, simsymbol, checkliving)
                 # only access the state if it isn't already in the dict
                 # (the foreign dict is cleared when $T is writeable).
                 if ! isempty(@agent($T).foreignstate)
-                    for i in 1:mpisize
-                        filter!(d -> haskey(@agent($T).foreignstate, d), perPE[i])
+                    for i in 1:mpi.size
+                        filter!(d -> ! haskey(@agent($T).foreignstate, d), perPE[i])
                     end
                 end
                 
@@ -233,7 +166,6 @@ function construct_mpi_agent_methods(T::DataType, attr, simsymbol, checkliving)
                 # for sending them via AllToAll we flatten the perPE structure
                 asvec = map(s -> collect(s), filter(! isempty, perPE))
 
-                
                 sendbuf = if ! isempty(asvec)
                     VBuffer(reduce(vcat, asvec),
                             [ length(perPE[i]) for i in 1:mpi.size ])
@@ -308,6 +240,75 @@ function construct_mpi_edge_methods(T::DataType, attr, simsymbol, CE)
     stateless = :Stateless in attr[:traits]
     
     ST = Vector{Tuple{AgentID, CE}}
+
+    # For sending the edges we need different versions, depending on the
+    # edge traits, as e.g. for the :HasEdgeOnly or :NumEdgeOnly we
+    # transfer only the number of edges. In overall, when we iterate over
+    # the container, we get the following values:
+    # |                    | Statel. | Ignore | get edges via | sending (ST below) |
+    # |--------------------+---------+--------+---------------+--------------------|
+    # | (Vector){Edge{$T}} |         |        | edges_to      | [(toid, Edge{$T})] |
+    # | (Vector){AgentID}  | x       |        | neighborids   | [(toid, fromid)]   |
+    # | (Vector){$T}       |         | x      | edgestates    | [(toid, $T)]       |
+    # | Int64              | x       | x      | num_neighbors | num_neighbors      |
+    @eval function sendedges!(sim, sendmap::Dict{AgentID, ProcessID}, idmapping, ::Type{$T})
+        if $singletype
+            AT = sim.typeinfos.edges_attr[$T][:to_agenttype]
+            typeID = sim.typeinfos.nodes_type2id[AT]
+        end
+
+        ST = Vector{Tuple{AgentID, sim.typeinfos.edges_attr[$T][:containerelement]}}
+        # We construct for each PE a vector with all the edges (incl. toid) that
+        # should be transmit to the PE
+        perPE = [ ST() for _ in 1:mpi.size ]
+
+        # The iterator for the edges depends on the traits of the edgetype
+        if $stateless && $ignorefrom
+            iter = @edgeread($T)
+            if $singletype 
+                iter = enumerate(iter)
+            end
+        else
+            iter = edges_iterator(sim, $T)
+        end
+
+        # we now iterate over the edges and add them to the perPE vector
+        # that corresponds to the process id given in the sendmap for the
+        # target of the edge
+        for (to, e) in iter
+            id = if $singletype
+                # we don't know the reuse value of the agent in the
+                # :SingleAgentType case, so we call immortal_agent_id,
+                # which uses the reuse value of 0.  This is okay, as we
+                # have removed the reuse bits from the AgentID already
+                # before in sendagents!
+                immortal_agent_id(typeID, AgentNr(to))
+            else
+                # To be consistent with sendagents!, we must remove
+                # the reuse bits here too
+                remove_reuse(to)
+            end
+            # in the default finish_init! case with an initialization
+            # that does not care about mpi.isroot, we have edges on
+            # all ranks, but on the sendmap there are only the edges
+            # of rank 0, as this are the only edges we want to distribute
+            if haskey(sendmap, id)
+                # in the SingleAgentType version, we also get entries with 0 edges
+                # we skip them, there is no need to use bandwith for that
+                if $stateless && $ignorefrom && e == 0
+                    continue
+                end
+                push!(perPE[sendmap[id]], (id, e))
+            end
+        end
+
+        # get the updated id of an agent with the old id `id`, whereby the
+        # reuse bits of `id` are set to zero (as this is also the case for
+        # the keys of idmapping)
+        updateid(id::AgentID) = idmapping[id] |> AgentID
+        
+        edges_alltoall!(sim, perPE, $T, updateid)
+    end
 
     # updateid is a func that update the ids of the agents, for the case
     # that the ids have changed in the distribution process.
