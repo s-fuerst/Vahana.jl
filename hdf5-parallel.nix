8c3bae3e1c8ffec73f478ab39d76c5a675e992a4
Steffen Fuerst <steffen.fuerst@gmx.de>
2025-03-05 16:34:31 +0100

    added nix files

---
+++ hdf5-parallel.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  libraries = with pkgs; [
+    hdf5-mpi
+    mpi
+  ];
+  libraryPath = pkgs.lib.makeLibraryPath libraries;
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    h5utils
+  ] ++ libraries;
+  
+  shellHook = ''
+    export LD_LIBRARY_PATH=${libraryPath}:$LD_LIBRARY_PATH
+    export OMPI_MCA_osc="^ucx"
+    julia --project -e 'using MPIPreferences; MPIPreferences.use_system_binary()'
+    echo "HDF5 (non parallel) environment activated"
+  '';
+}
